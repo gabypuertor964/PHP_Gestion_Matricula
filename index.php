a75e399077058d7eb8328a214a48a781ed4cc195
gabypuertor964 <gabrielspuertor57@gmail.com>
2023-05-31 08:26:42 -0500

    nombres Archivos

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <!-- Bootstrap CSS v5.2. -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="addons/estiloindex.css">
+    <link rel="stylesheet" href="addons/style.css">
 
 </head>
 

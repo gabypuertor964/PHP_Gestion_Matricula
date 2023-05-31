899f3473ee462d89e7d3d8f82fb79cadc12c165c
gabypuertor964 <gabrielspuertor57@gmail.com>
2023-05-31 08:18:48 -0500

    xd

--- model/conexion.php
+++ model/conexion.php
@@ -12,4 +12,11 @@
    //variable que contiene los datos para la conexion
    $con = new mysqli($servidorBD, $usuarioBD, $contrasenaBD, $nombreBD);
 
+   if ($con->connect_errno) {
+      echo "Fallo al conectar a MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+  }
+  
+  echo $con->host_info . "\n";
+  ?>
+
 ?>

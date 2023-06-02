dd56922b71fa7fccc7e094b0771b2df2f81849e3
gabypuertor964 <gabrielspuertor57@gmail.com>
2023-06-02 07:00:37 -0500

    Eliminacion prefix

--- views/header.php
+++ views/header.php
@@ -15,7 +15,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
         <!--Link Hoja de Estios Propia-->
-        <link rel="stylesheet" href="<?php echo($_SESSION['prefix'])?>addons/style.css">
+        <link rel="stylesheet" href="../addons/style.css">
 
     </head>
 
